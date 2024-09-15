bfde8b238b8b7c151be0af142cb88a1ea834451c
sergey <sergey@sergey.sergey>
2024-09-15 21:30:06 +0100

    fix

--- emails_project/emails/views.py
+++ emails_project/emails/views.py
@@ -3,5 +3,4 @@ from .models import EmailMessage
 
 
 def message_list(request):
-    messages = EmailMessage.objects.all()
-    return render(request, "email_list.html", {"messages": messages})
+    return render(request, "email_list.html")
